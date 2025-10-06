ca35b5b37e45d81c8a5c879f38657f42a47f9be8
David Poindexter <davidpoindexter03@gmail.com>
2025-10-05 19:29:56 -0700

    route functionality

--- blockchain.py
+++ blockchain.py
@@ -5,7 +5,7 @@ from time import time
 from uuid import uuid4
 from textwrap import dedent
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -68,11 +68,39 @@ blockchain = Blockchain()
 
 @app.route('/main', methods=['GET'])
 def mine():
-    return 'mine'
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender=0,
+        recipient=node_identifier,
+        amount=1
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block Created',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response, 200)
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transactions():
-    return 'new_transactions'
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing required values', 400
+    
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
